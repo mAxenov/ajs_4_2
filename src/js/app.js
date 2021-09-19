8d638403ea3860643d202643364b02156ed6d8f3
Maxim <maxime.axenoff@yandex.ru>
2021-09-19 20:48:37 +0500

    WebPack2

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,6 @@
 "use strict"
 
-console.log('app worked')
+import GameSavingData, { Game, readGameSaving as loadGame, writeGameSaving as saveGame } from './game.js';
+
+const game = new Game();
+game.start();
